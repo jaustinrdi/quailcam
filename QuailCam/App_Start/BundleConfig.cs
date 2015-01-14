f63a6e33daae745d162526bd6c86f0ec722c8c73
jaustinrdi <jaustin@resdat.com>
2015-01-14 14:07:35 -0900

    Add chart framework using chart.js

--- QuailCam/App_Start/BundleConfig.cs
+++ QuailCam/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace QuailCam
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+                "~/Scripts/Chart.js",
+                "~/Scripts/readingCharts.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ QuailCam/Scripts/readingCharts.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    var chartCanvas = $("#lightChart");
+    var ctx = chartCanvas.get(0).getContext("2d");
+    var chart = new Chart(ctx).Line(lightData);
+    // TODO: add legend somewhere
+});
